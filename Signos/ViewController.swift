e2728aec5a56c8860af20cff35b05901de65d4bf
jiraspiom <jiraspiom@yahoo.com.br>
2019-04-07 10:01:53 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Gilmar Borges on 07/04/19.
+//  Copyright © 2019 alalgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
